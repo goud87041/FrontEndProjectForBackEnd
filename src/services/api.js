96e7f40714e4e2b1f81fd478d725dd6e49501a6d
abhishek goud <goud87041@gmail.com>
2026-02-16 17:52:56 +0530

    add some bug for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URl = import.meta.env.VITE_API_URL
+
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: BASE_URl,
   withCredentials: true, // VERY IMPORTANT (cookies)
 });
 
