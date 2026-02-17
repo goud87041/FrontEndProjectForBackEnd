9ea6b7b1a8b31ccb4c822e008ccedb31064d20d4
abhishek goud <goud87041@gmail.com>
2026-02-17 09:38:24 +0530

    update api baseURL to include versioning and local endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URl = import.meta.env.VITE_API_URL
 
 const api = axios.create({
-  baseURL: BASE_URl,
+  baseURL: [BASE_URl.api/v1,"http://localhost:4000"],
   withCredentials: true, // VERY IMPORTANT (cookies)
 });
 
